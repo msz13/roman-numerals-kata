fd02a97c741ff974544c844f572cba43c2991e76
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-14 23:22:08 +0000

    8 ran tests, no prediction, got amber

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -4,4 +4,10 @@ public class RomanConverter
     {
         return "I" * number;
     }
+    
+    private static Dictionary<int,string> Map = new Dictionary<int, string>() {
+        {1, "I"},
+        {2, "II"},
+        {3, "III"}
+    }
 }
