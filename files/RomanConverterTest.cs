5bb6d8a3c6a1d50f7650ac571b7ce951b53c6d4a
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 19:43:54 +0000

    37 ran tests, no prediction, got red

--- files/RomanConverterTest.cs
+++ files/RomanConverterTest.cs
@@ -8,7 +8,7 @@ public class HikerTest
     [TestCase(3, "III")]
     [TestCase(3, "III")]
     [TestCase(5, "V")]
-    //[TestCase(10, "X")]
+    [TestCase(10, "X")]
     //[TestCase(50, "L")]
     //[TestCase(100, "C")]
     //[TestCase(500, "D")]
