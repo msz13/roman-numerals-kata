098c7bdecd12507991da44082dc9a901496c7fbe
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 22:40:02 +0000

    116 ran tests, no prediction, got red

--- files/RomanConverterTest.cs
+++ files/RomanConverterTest.cs
@@ -3,9 +3,9 @@ using NUnit.Framework;
 [TestFixture]
 public class HikerTest
 {
-  /*  [TestCase(1, "I")]
+    [TestCase(1, "I")]
     [TestCase(2, "II")]
-     [TestCase(3, "III")]
+    [TestCase(3, "III")]
     [TestCase(3, "III")]
     [TestCase(4, "IV")]
     [TestCase(5, "V")]
@@ -22,8 +22,8 @@ public class HikerTest
     [TestCase(63, "LXIII")]    
     //[TestCase(100, "C")]
     //[TestCase(500, "D")]
-    //[TestCase(1000, "M")] */
-    [TestCase(4, "IV")]
+    //[TestCase(1000, "M")] 
+   
     public void should_convert_arabic_to_Roman(int arabic, string roman)
     {
         // a simple example to start you off
