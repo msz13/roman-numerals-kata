11fe6fbfe6a644045e23afe2dc81a88aa89897c5
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 21:43:30 +0000

    78 ran tests, no prediction, got amber

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -4,34 +4,43 @@ using System;
 
 public class RomanConverter
 {
+    StringBuilder _sb = new StringBuilder("");        
+    int _reminder;
+    
     public static string ToRoman(int arabic)
     {
-        var sb = new StringBuilder("");
-        
-        int reminder;
+               
+       _reminder = arabic;
        
-        var tens = Math.DivRem(reminder, 10, out reminder);
+        appendOrderOfMagnitude(_reminder, 50)
+        appendOrderOfMagnitude(_reminder, 10)
+        appendOrderOfMagnitude(_reminder, 5)
+        appendOrderOfMagnitude(_reminder, 1)
+            
+            
+        /* var tens = Math.DivRem(reminder, 10, out reminder);
         var fives = Math.DivRem(reminder, 5, out reminder); 
                          
         sb.Append('L', fifties);
         sb.Append('X',tens);            
         sb.Append('V', fives);
-        if (reminder == 4 ) 
+      
+          if (reminder == 4 ) 
         {
           sb.Append('I', 1);
           sb.Append('V', 1);
         } else 
         {
         sb.Append('I', reminder);    
-        }
+        } */
         
         return sb.ToString();
     }  
     
-    private AppendOrderOfMagnitude(orderOfMagnitude) 
+    private static void appendOrderOfMagnitude(int value, int orderOfMagnitude) 
     {
-      var fifties = Math.DivRem(arabic, 50, out reminder); 
-       sb.Append(symbols[orderOfMagnitude], fifties);
+      var result = Math.DivRem(_reminder, orderOfMagnitude, out _reminder); 
+       sb.Append(symbols[orderOfMagnitude], result);
     }
     
     
