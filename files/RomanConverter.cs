c7051cad47a38f3088a4e3488ec5b96541701737
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 19:15:00 +0000

    24 ran tests, no prediction, got green

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -5,7 +5,7 @@ public class RomanConverter
 {
     public static string ToRoman(int arabic)
     {
-        return new StringBuilder().Insert(0, symbols[1], arabic);
+        return new StringBuilder().Insert(0, symbols[1], arabic).ToString();
     }
     
     private static Dictionary<int,string> symbols = new Dictionary<int, string>() {
