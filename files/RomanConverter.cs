4c0400a735cb4bad6a5c2f6ce7b8fbfe809d2a46
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 20:38:11 +0000

    67 ran tests, no prediction, got green

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -13,6 +13,7 @@ public class RomanConverter
         var tens = Math.DivRem(reminder, 10, out reminder);
         var fives = Math.DivRem(reminder, 5, out reminder);      
                  
+        sb.Append('L', fifties);
         sb.Append('X',tens);            
         sb.Append('V', fives);
         sb.Append('I', reminder);    
