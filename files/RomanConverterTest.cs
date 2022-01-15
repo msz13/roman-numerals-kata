51a4f43deb28fae6f61f4172e522ad8682cbb546
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 22:00:12 +0000

    93 ran tests, no prediction, got green

--- files/RomanConverterTest.cs
+++ files/RomanConverterTest.cs
@@ -5,7 +5,7 @@ public class HikerTest
 {
     [TestCase(1, "I")]
     [TestCase(2, "II")]
-   /*   [TestCase(3, "III")]
+     [TestCase(3, "III")]
     [TestCase(3, "III")]
    // [TestCase(4, "IV")]
     [TestCase(5, "V")]
@@ -19,11 +19,10 @@ public class HikerTest
     [TestCase(20, "XX")]
     [TestCase(30, "XXX")]
     [TestCase(50, "L")]
-    [TestCase(63, "LXIII")]
-    //[TestCase(50, "L")]
+    [TestCase(63, "LXIII")]    
     //[TestCase(100, "C")]
     //[TestCase(500, "D")]
-    //[TestCase(1000, "M")] */
+    //[TestCase(1000, "M")] 
     public void should_convert_arabic_to_Roman(int arabic, string roman)
     {
         // a simple example to start you off
