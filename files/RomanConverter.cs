ab1540f6f8a097de0ded2bd65bcf6117c6c0e642
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 19:01:21 +0000

    14 ran tests, no prediction, got red

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -4,7 +4,7 @@ public class RomanConverter
 {
     public static string ToRoman(int number)
     {
-        return new string(symbols[numbers]);
+        return new string(symbols[number]);
     }
     
     private static Dictionary<int,string> symbols = new Dictionary<int, string>() {
