a34030b921691763e6d9cc9054edc3996020481c
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 20:42:02 +0000

    70 ran tests, no prediction, got amber

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -11,13 +11,19 @@ public class RomanConverter
         int reminder;
         var fifties = Math.DivRem(arabic, 50, out reminder); 
         var tens = Math.DivRem(reminder, 10, out reminder);
-        var fives = Math.DivRem(reminder, 5, out reminder);      
-                 
+        var fives = Math.DivRem(reminder, 5, out reminder); 
+                         
         sb.Append('L', fifties);
         sb.Append('X',tens);            
         sb.Append('V', fives);
+        if (reminder == 4 ) 
+        {
+        sb.Append('IV', 1)
+        } else 
+        {
         sb.Append('I', reminder);    
-             
+        }
+        
         return sb.ToString();
     }          
            
