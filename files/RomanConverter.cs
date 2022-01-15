337550865f3cee60bf4c7fc3ba122067f8138d80
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-15 19:10:18 +0000

    21 ran tests, no prediction, got amber

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -4,7 +4,7 @@ public class RomanConverter
 {
     public static string ToRoman(int arabic)
     {
-        return new string(symbols[1], arabic);
+        return new StringBuilder().Insert(0, symbols[1], arabic);
     }
     
     private static Dictionary<int,string> symbols = new Dictionary<int, string>() {
