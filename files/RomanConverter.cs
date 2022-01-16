056863c75b4bd1b7452ef4bca0d55c835bede2a9
axC4Rj <axC4Rj@cyber-dojo.org>
2022-01-16 15:37:32 +0000

    178 ran tests, no prediction, got amber

--- files/RomanConverter.cs
+++ files/RomanConverter.cs
@@ -17,7 +17,7 @@ public class RomanConverter
     public static string ToRoman(int arabic)
     {                            
               
-       return new RomanConverter(arabic).AppendUnits().GetResult();
+       return new RomanConverter(arabic).AppendTens().AppendUnits().GetResult();
         
     }  
     
@@ -45,6 +45,30 @@ public class RomanConverter
        }
         
        _sb.Append(symbols[1], localReminder);          
+                       
+       return this;
+    }
+    
+     RomanConverter AppendTens()
+    {
+      /*   if(isSubstractive(90,40)) 
+        {
+            _sb.Append(substractive[_reminder]);
+            _reminder = 0;
+             return this;
+        } */
+        
+       int localReminder; 
+       var result = Math.DivRem(_reminder, 50, out localReminder);  
+        
+      if(result == 1) 
+       {
+        _sb.Append(symbols[50]);
+       } 
+        
+       _sb.Append(symbols[10], result);
+         
+       _reminder = localReminder
                        
        return this;
     }
